e56a3d15296331de1057edbd06f0b692080d6857
xx6MarcianoEstudador9xx <164871749+senhorN2@users.noreply.github.com>
2024-10-07 11:15:23 -0300

    bf

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Teste de conhecimento(1)',
+    'O nathan é gay?',
+    'sim'
+)
+criaCartao(
+    'Teste de conhecimento(2)',
+    'O nathan é retardado?',
+    'sim'
+)
+criaCartao(
+    'Teste de conhecimento(3)',
+    'O nathan da o cu?',
+    'sim'
+)
+criaCartao(
+    'Teste de conhecimento(4)',
+    'O nathan é adotado?',
+    'sim'
+)
+criaCartao(
+    'Teste de conhecimento(5)',
+    'O nathan sabe japonês?',
+    'sim'
+)
+criaCartao(
+    'Teste de conhecimento(6)',
+    'O nathan ?',
+    'sim'
+)
